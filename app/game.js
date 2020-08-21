2a08dd874bf9ff982c69d509b06cebea61fb92e8
Kévin Rocha <neonix75@gmail.com>
2020-08-21 10:04:14 +0200

    setting up the game logique

--- app/game.js
+++ app/game.js
@@ -12,3 +12,52 @@ const columns = canvas.width / scale;
 
 let snake;
 let apple;
+
+// Run the game by updating and drawing the snake and the fruit
+function main() {
+    snake = new Snake();
+    apple = new Apple();
+
+    snake.draw();
+    apple.randomPosition();
+
+    window.setInterval(() => {
+        displayScore.innerHTML = `score: ${snake.totalAppleEaten}`;
+        
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        
+        apple.draw();
+
+        snake.update();
+        snake.draw();
+
+        if(snake.eatApple(apple)) {
+            apple.randomPosition();
+        }
+
+        if(snake.checkCollision()) {
+            alert(`Perdu\nVotre score: ${snake.totalAppleEaten}`);
+            snake.totalAppleEaten = 0;
+            snake.tail = [];
+        }
+
+
+    }, 80);
+}
+main();
+
+
+
+// Change the direction when a key is down
+window.addEventListener("keydown", (event) => {
+    const direction = event.key.replace("Arrow", "");
+    
+    snake.changeDirection(direction);
+});
+
+
+
+
+
+
+export { canvas, ctx, scale, snake, apple, rows, columns };
